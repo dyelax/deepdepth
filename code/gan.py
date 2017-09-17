41410209b539ba256fb0f62e99f96d16a5ee1648
Matt Cooper <matthew_cooper@brown.edu>
2017-09-17 08:06:18 -0400

    printing inputs and labels

--- code/gan.py
+++ code/gan.py
@@ -99,6 +99,9 @@ def img_reader():
         d_image = d_image.crop((left, top, right, bottom))
         rgb_image = rgb_image.crop((left, top, right, bottom))
 
+        d_image.save('/mnt/results/depth.jpg')
+        rgb_image.save('/mnt/results/rgb.jpg')
+
         depth_arr = np.array(d_image).flatten()
         rgb_arr = np.array(rgb_image).flatten()
 
